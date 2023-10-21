83ea32343f02104115c35ec0c31056bb7a43c770
quanganh <nguyenquanganh16012000@gmail.com>
2023-10-21 15:21:05 +0700

    update

---
+++ src/tools/index.js
@@ -0,0 +1,14 @@
+import jwt from "jsonwebtoken";
+
+const tools = {
+  gennerateToken: (payload, secretKey, expiresIn) => {
+    const token = jwt.sign(payload, secretKey, { expiresIn });
+    return token;
+  },
+  gennerateRefreshToken: (payload, secretKey, expiresIn) => {
+    const refreshoken = jwt.sign(payload, secretKey, { expiresIn });
+    return refreshoken;
+  }
+};
+
+module.exports = tools;
